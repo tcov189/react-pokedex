96f43c1cb4e7595b61c6aa3f61dfa438c5b2e773
Trevor Covington <trevor.covington@travelctm.com>
2021-05-12 16:51:31 -0500

    End 3

--- src/App.js
+++ src/App.js
@@ -5,21 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HelloWorld name="Trevor" />
+        <HelloWorld name="World" />
       </header>
     </div>
   );
+}
+
+function HelloWorld(props) {
+  return <h1>Hello {props.name}!</h1>
 }
 
 export default App;
